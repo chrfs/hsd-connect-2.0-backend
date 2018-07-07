37105b441e55be4759195fad039987d483bc2b4c
Abdelmoez Cherif <a.cherif@me.com>
2018-07-07 11:22:34 +0200

    updated .gitignore

--- hsd-connect-2.0-backend/README.md
+++
@@ -1,5 +0,0 @@
-# HSD CONNECT
-
-[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
-
-Plattform to find Projects and Collaborations
--- hsd-connect-2.0-backend/src/routes/index.mjs
+++
@@ -1,7 +0,0 @@
-import KoaRouter from 'koa-router';
-import env from '../config/env.mjs';
-
-const router = new KoaRouter();
-((async() => router.use((await import(`.${env.API.PATH}`)).default.routes()))());
-
-export default router;
